7b3dd3cb7ab547c7e04b05ffe01b6b30fe29600e
ninzindaisuki <takuma.osakakochi@gmail.com>
2025-06-04 16:16:41 +0100

    20250604pwmモジュール慣性，姿勢推定ライブラリ追加

--- RaspberryPi/ex/PCA9685/pca9685.py
+++ RaspberryPi/ex/PCA9685/pca9685.py
@@ -4,6 +4,16 @@ import math
 
 pwm_addr=0x40
 
+LED0_ON_L          = 0x06
+LED0_ON_H          = 0x07
+LED0_OFF_L         = 0x08
+LED0_OFF_H         = 0x09
+
+ALL_LED_ON_L       = 0xFA
+ALL_LED_ON_H       = 0xFB
+ALL_LED_OFF_L      = 0xFC
+ALL_LED_OFF_H      = 0xFD
+
 class PCA9685():
     def __init__(self,module):
         # define i2c module
@@ -17,6 +27,7 @@ class PCA9685():
         mode1=mode1 & ~0x10
         self.i2c.write_byte_data(pwm_addr,0x00,mode1)
         time.sleep(0.01)
+        self.set_all_pwm(0,0)
     
     def set_pwm_freq(self,freq_hz):
         prescaleval = 25000000.0    # 25MHz
@@ -34,3 +45,31 @@ class PCA9685():
         time.sleep(0.01)
         self.i2c.write_byte_data(pwm_addr,0x00,(oldmode | 0x80))
         time.sleep(0.01)
+
+    def set_pwm(self,channel,on,off):
+        self.i2c.write_byte_data(pwm_addr,LED0_ON_L+4*channel,(on & 0xFF))
+        self.i2c.write_byte_data(pwm_addr,LED0_ON_H+4*channel,(on >> 8))
+        self.i2c.write_byte_data(pwm_addr,LED0_OFF_L+4*channel,(off & 0xFF))
+        self.i2c.write_byte_data(pwm_addr,LED0_OFF_H+4*channel,(off >> 8))
+
+    def set_all_pwm(self,on,off):
+        self.i2c.write_byte_data(pwm_addr,ALL_LED_ON_L,(on & 0xFF))
+        self.i2c.write_byte_data(pwm_addr,ALL_LED_ON_H,(on >> 8))
+        self.i2c.write_byte_data(pwm_addr,ALL_LED_OFF_L,(off & 0xFF))
+        self.i2c.write_byte_data(pwm_addr,ALL_LED_OFF_H,(off >> 8))
+
+i2c=smbus.SMBus(1)
+pca9685=PCA9685(i2c)
+
+pca9685.set_pwm_freq(490)
+pca9685.set_pwm(0,0,3000)
+time.sleep(0.1)
+print()
+pca9685.set_pwm(0,0,200)
+time.sleep(0.1)
+
+try :
+    while True:
+        pca9685.set_pwm(0,0,2000)
+except KeyboardInterrupt:
+    pca9685.set_all_pwm(0,0)
