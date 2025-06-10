937b60ec72e2a8f5e18a2757d89de12688e60ff9
ninzindaisuki <takuma.osakakochi@gmail.com>
2025-06-11 00:05:34 +0100

    20250611朝活

--- RaspberryPi/src/sensor.py
+++ RaspberryPi/src/sensor.py
@@ -1,12 +1,32 @@
 import socket
 import time
 import pickle
-from ADDRES import Sensor_node
+from ADDRES import Sensor_node,PC_node
+
+from lib/icm20948 import icm20948
 
 NodeAddres=Sensor_node()
+PC=PC_node()
 
 # UDP通信クライアント設立
 client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 client.bind(NodeAddres.addres)
 
-# 送信先アドレス
+def main_worker():
+    # センサ値取得
+    data=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    # PCへ送信
+    Send_Data=pickle.dumps(data)
+    client.sendto(Send_Data,PC.addres)
+
+# 定周期大麻
+def scheduler(interval, func):
+    base_time = time.time()
+    next_time = 0
+    while True:
+        func()
+        next_time = ((base_time - time.time()) % interval) or interval
+        time.sleep(next_time)
+
+
+scheduler(0.01,main_worker)
