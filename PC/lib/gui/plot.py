9082135e1625c64668d4b5430becdf6cdb534fd8
ninzindaisuki <takuma.osakakochi@gmail.com>
2025-06-11 20:35:48 +0900

    20250612dev_aにプッシュ

--- PC/lib/gui/plot.py
+++ PC/lib/gui/plot.py
@@ -1,13 +1,3 @@
-import socket
-import time
-import pickle
-
-from PyQt6.QtWidgets import QApplication
-
-from lib import Propo as Propo
-from lib.gui import plot
-from ADDRES import Sensor_node,Camera_node,Controler_node,PC_node
-
 import pyqtgraph as pg
 import os
 
@@ -17,50 +7,11 @@ from PyQt6.QtCore import QTimer
 
 import math 
 
-#登場するノードを定義
-Sensor=Sensor_node()
-Camera=Camera_node()
-ECU=Controler_node()
-PC=PC_node()
-
-#コントローラー初期化
-propo=Propo.ps4()
-PropoData=[0]*4
-
-# UDP通信クライアント設立
-client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-client.bind(PC.addres)
-
-lasttime=0
-# メイン制御部
-def main_worker():
-    message=1
-    # センサ値取得
-    data,addr=client.recvfrom(1024)
-    sensorData=pickle.loads(data)
-    print(sensorData)
-    # コントローラ値取得
-    # PropoData=propo.getPropoData()
-    # 制御計算
-    # motorinput=PID.calc_input(PropoData)
-    # ECUノードへ送信
-    # SendData=pickle.dumps(PropoData)
-    # client.sendto(SendData,ECU.addres)
-    # print(PropoData)
-    return sensorData
-
-
-
 class mainwindow(QMainWindow):
     def __init__(self):
         super().__init__()
         ui_path=os.path.join(os.path.dirname(__file__),"data_check.ui")
         uic.loadUi(ui_path,self)
-
-        # 大麻設定
-        self.timer=QTimer()
-        self.timer.timeout.connect(self.update)
-        self.timer.start(10)   
 
         # roll プロッタ設定
         self.plot1_config=self.roll_plot.addPlot()
@@ -82,10 +33,10 @@ class mainwindow(QMainWindow):
         self.x=[]
         self.y=[]
 
-    def update(self):
-        plotdata=main_worker()
+    def update(self,function):
+        function()
         self.x.append(self.time)
-        self.y.append(plotdata[0])
+        self.y.append(self.ydash)
         self.plot1_data.setData(self.x[-500:],self.y[-500:])
         self.plot2_data.setData(self.x[-500:],self.y[-500:])
         self.plot3_data.setData(self.x[-500:],self.y[-500:])
@@ -96,11 +47,19 @@ class mainwindow(QMainWindow):
             self.plot2_config.setXRange(self.time-500,self.time+50,padding=0)
             self.plot3_config.setXRange(self.time-500,self.time+50,padding=0)
 
-         
-app=QApplication([])
-main=mainwindow()
-main.show()
-app.exec()    
+    def set_timer(self,function):
+        # 大麻設定
+        self.timer=QTimer()
+        self.timer.timeout.connect(self.update(function))
+        self.timer.start(10)    
+
+def gui_start():
+    app=QApplication([])
+    main=mainwindow()
+    main.show()
+    app.exec()    
+
+
         
 
 
