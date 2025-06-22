2904ee2846062225c50fb5dfcadb1a6fe2754c86
ninzindaisuki <takuma.osakakochi@gmail.com>
2025-06-23 08:08:30 +0900

    ipアドレス自動認識２

--- ADDRES.py
+++ ADDRES.py
@@ -1,3 +1,59 @@
+import socket
+import time
+
+def CheckIPAddress(module):
+    if module=="PC":
+        ip=socket.gethostbyname("raspberrypi.local")
+        print(f"Found RaspberryPi. The IP Address is {ip} !!")
+        client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        client.connect((ip,5000))
+        thisIP=client.getsockname()[0]
+        print(f"This PC's IP is {thisIP} !!")
+        client.close()
+
+        # 通信チェック用のクライアント立ち上げ
+        client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        client.bind((thisIP,5000))
+        data=""
+        while True:
+            #通信確認用メッセージ送信
+            msg="HelloRaspberryPi!!"
+            SendData=msg.encode("utf-8")
+            client.sendto(SendData,(ip,5000))
+            print("Check Connect...")
+            data,addr=client.recvfrom(1024)
+            time.sleep(2)
+            
+            if data.decode("utf-8")=="HelloPC!!":
+                # 終了処理
+                client.close()
+                break
+        print("socket com is OK.")
+        print(f"RaspberryPi's IP is {addr}")
+        return addr
+    
+    elif module=="RaspberryPi":
+        client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        client.bind(("",5000))
+        data=""
+        while True:
+            data,addr=client.recvfrom(1024)
+            print("Check Connect...")
+            if data.decode("utf-8")=="HelloRaspberryPi!!":
+                # 返送用メッセージ送信
+                msg="HelloPC!!"
+                SendData=msg.encode("utf-8")
+                client.sendto(SendData,addr)
+                break
+        client.close()
+        print("socket com is OK.")
+        print(f"PC's IP is {addr}")
+        return addr
+    
+    else:
+        print("The augument is invalid. Set PC or RaspberryPi")
+
+
 class Sensor_node:
     def __init__(self):
         self.IP="169.254.143.180"
--- PC/lib/socket/try.py
+++ PC/lib/socket/try.py
@@ -5,11 +5,4 @@ import socket
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../..")))
 import ADDRES
 
-ip=socket.gethostbyname("raspberrypi.local")
-print(f"Found RaspberryPi. The IP Address is {ip} !!")
-
-client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-client.connect((ip,5000))
-thisIP=client.getsockname()[0]
-
-print(thisIP)
+ADDRES.CheckIPAddress("PC")
