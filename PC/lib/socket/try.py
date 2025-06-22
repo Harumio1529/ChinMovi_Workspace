55528a8006879d4f65ac1286dd5c3d98b509d12f
ninzindaisuki <takuma.osakakochi@gmail.com>
2025-06-22 22:50:28 +0100

    ipアドレス自動認識

--- PC/lib/socket/try.py
+++ PC/lib/socket/try.py
@@ -1,4 +1,15 @@
+import sys
+import os
 import socket
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../..")))
+import ADDRES
+
 ip=socket.gethostbyname("raspberrypi.local")
-print(type(ip))
+print(f"Found RaspberryPi. The IP Address is {ip} !!")
+
+client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+client.connect((ip,5000))
+thisIP=client.getsockname()[0]
+
+print(thisIP)
