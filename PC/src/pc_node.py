9b6d21600d099ecba8778c7904d96d6bd2c3a6fa
ninzindaisuki <takuma.osakakochi@gmail.com>
2025-07-13 21:34:28 +0900

    コミット

--- PC/src/pc_node.py
+++ PC/src/pc_node.py
@@ -18,13 +18,29 @@ from lib.gui.plot import mainwindow
 # 通信用顧問
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../..")))
 import COMMON
+SA=COMMON.StatusAnalyzer()
+
+# デバッグモード
+DEBUG_MODE=False
+
 # 通信チェック
-RasPi_IP,PC_IP=COMMON.CheckIPAddress("PC")
+# デバッグモードなら通信チェックなし
+if DEBUG_MODE:
+    RasPi_IP="0.0.0.0"
+    PC_IP="0.0.0.0"
+else:    
+    RasPi_IP,PC_IP=COMMON.CheckIPAddress("PC")
 
-# デバッグ用ダミーIPアドレス
-# RasPi_IP="0.0.0.0"
-# PC_IP="0.0.0.0"
 
+# 受信データコンテナ
+acc=[0]*3
+gyr=[0]*3
+eul=[0]*3
+STSOCKET=""
+STIMU=""
+STTHRUST=""
+STSERVO=""
+STCHU=""
 
 #コントローラー初期化
 propo=ps4()
@@ -33,16 +49,45 @@ PropoData=[0]*4
 # UDP通信クライアント設立
 ComAgent=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 ComAgent.bind((PC_IP,COMMON.PCPort))
-ComAgent.settimeout(0.0001)
+ComAgent.settimeout(1)
 
+def data_separeter(data):
+    global acc,gyr,eul,STSOCKET,STIMU,STTHRUST,STSERVO,STCHU
+    acc=data[0:3]
+    gyr=data[3:6]
+    eul=data[6:9]
+    [STSOCKET,STIMU,STTHRUST,STSERVO,STCHU]=SA.Decoder(data[9:])
+
+def status_controler():
+    global STSOCKET,STIMU,STTHRUST,STSERVO,STCHU
+    # ステータスを監視して、全部ReadyならWorkingに遷移してもらう
+    if STIMU=="READY" and STTHRUST=="READY" and STSERVO=="READY" and STCHU=="READY":
+        STIMU="WORKING"
+        STTHRUST="WORKING"
+        STSERVO="WORKING"
+        STCHU="WORKING"
+    # ステータスのエンコード
+    return SA.Encoder([STSOCKET,STIMU,STTHRUST,STSERVO,STCHU])
+
+    
+    
 
 def Com_main():
     global PropoData
     try:
+        # データ受信
         data,addr=ComAgent.recvfrom(1024)
+        # 受信データ分割
+        data_separeter(pickle.loads(data))
+        # ステータス監視
+        EncodeStatus=status_controler()
+        # プロポデータ取得
         PropoData=propo.getPropoData()
-        ComAgent.sendto(pickle.dumps(PropoData),(RasPi_IP,COMMON.RasPiPort))
-        print(pickle.loads(data))
+        # 送信データ作成
+        SendData=[*PropoData,*EncodeStatus]
+        ComAgent.sendto(pickle.dumps(SendData),(RasPi_IP,COMMON.RasPiPort))
+        # print(pickle.loads(data))
+        print([STSOCKET,STIMU,STTHRUST,STSERVO,STCHU])
     
     except socket.timeout:
         print("timeout")
@@ -51,33 +96,6 @@ def Com_main():
 
 
 COMMON.scheduler(0.01,Com_main)
-
-
-
-lasttime=0
-# メイン制御部
-def main_worker():
-    message=1
-    # センサ値取得
-    # data,addr=client.recvfrom(1024)
-    # sensorData=pickle.loads(data)
-    # print(sensorData)
-    # コントローラ値取得
-    # PropoData=propo.getPropoData()
-    # ECUノードへ送信
-    # SendData=pickle.dumps(PropoData)
-    # client.sendto(SendData,ECU.addres)
-    # print(PropoData)
-    # return sensorData
-
-
-
-
-         
-# app=QApplication([])
-# main=mainwindow(main_worker)
-# main.show()
-# app.exec()    
         
 
 
